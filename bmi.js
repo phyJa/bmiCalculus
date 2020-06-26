54c39140c04a36e3e2b0bc7ba9a022ae26dd4de7
Elias <eli.bueno9@gmail.com>
2020-06-26 08:59:28 -0300

    Conditional ready

--- bmi.js
+++ bmi.js
@@ -1,11 +1,23 @@
 //Get the values from the user
 function calculate() {
-    var message;
+    //Preparing the messages
+    var message, message2 ;
     message = document.getElementById("answer");
+    message2 = document.getElementById("answer2");
     message.innerHTML = "";
+    message2.innerHTML = "";
+    //Declaring the constants
     const height = document.getElementById("height").value;
     const mass = document.getElementById("mass").value;
     const bmi = mass / (height * height);
-    message.innerHTML = `Your BMI is ${bmi}.`
-    
+    //Printing the messsages
+    message.innerHTML = `Your BMI is ${bmi}.`;
+    //Stablish the conditional
+    if(bmi > 30) {
+        message2.style.color = "red";
+        message2.innerHTML = "Warning! You are overweight.";
+    } else {
+        message2.style.color = "blue";
+        message2.innerHTML = "You are not overweight.";
+    }
 }
