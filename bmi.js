344e803c6d4cdef5cdf30656de50c41a6d60d64c
Elias <eli.bueno9@gmail.com>
2020-07-08 09:46:48 -0300

    Starting to insert retirement input fields

--- bmi.js
+++ bmi.js
@@ -9,16 +9,21 @@
 //Get the values from the user
 function calculate() {
     //Preparing the messages
-    var message, message2 ;
+    var message, message2, message3 ;
     message = document.getElementById("answer");
     message2 = document.getElementById("answer2");
+    message3 = document.getElementById("answer3");
     message.innerHTML = "";
     message2.innerHTML = "";
+    message3.innerHTML = "";
     //Declaring the constants
     const personData = {
         name: document.getElementById("name").value,
         height: document.getElementById("height").value,
         mass: document.getElementById("mass").value,
+        age: document.getElementById("mass").age,
+        timeOfWork: document.getElementById("time_of_work").value,
+        gender: document.getElementsByName("gender").value,
         bmi: function() {
             return this.mass / (this.height * this.height);
         }
@@ -33,4 +38,12 @@ function calculate() {
         message2.style.color = "blue";
         message2.innerHTML = "You are not overweight.";
     }
+    //Answers of retirement
+    message3.innerHTML = personData.gender;
+    // if(personData.gender === "male") {
+    //     message3.innerHTML = "You are male";
+    // } else {
+    //     message3.innerHTML = "You are female";
+    // }
+    
 }
