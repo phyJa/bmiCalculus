79290131578592afc4b061ee9d3b91ee585449ed
Elias <eli.bueno9@gmail.com>
2020-07-09 10:08:37 -0300

    Retirement script almost ready

--- js/retirement.js
+++ js/retirement.js
@@ -1,40 +1,40 @@
-//Display the answers
-    //First, store the element in a variable
-    let answers = document.querySelector("#answers");
-    //Add an event listener to the button
-    let buttonListener = document.querySelector("form button");
-    //Remove the class "notDisplaying" from the answers
-    buttonListener.addEventListener("click", () => {answers.classList.remove("notDisplaying");});
-
-//Get the values from the user
-function calculate() {
-    //Preparing the messages
-    var message, message2, message3 ;
-    message = document.getElementById("answer");
-    message2 = document.getElementById("answer2");
-    message3 = document.getElementById("answer3");
-    message.innerHTML = "";
-    message2.innerHTML = "";
-    message3.innerHTML = "";
-    //Declaring the constants
-    const personData = {
-        name: document.getElementById("name").value,
-        height: document.getElementById("height").value,
-        mass: document.getElementById("mass").value,
-        age: document.getElementById("age").age,
-        timeOfWork: document.getElementById("time_of_work").value,
-        bmi: function() {
-            return this.mass / (this.height * this.height);
-        }
-    };
-    //Printing the messsages
-    message.innerHTML = `Hello, ${personData.name}! Your BMI is ${personData.bmi()} kg/m<sup>2</sup>.`;
-    //Stablishing the conditional
-    if(personData.bmi() > 30) {
-        message2.style.color = "red";
-        message2.innerHTML = "Warning! You are overweight.";
-    } else {
-        message2.style.color = "blue";
-        message2.innerHTML = "You are not overweight.";
+const persons = [
+    {
+        name: "John",
+        age: 40,
+        contribution: 35,
+        gender: "Male"
+    },
+    {
+        name: "Mary",
+        age: 40,
+        contribution: 35,
+        gender: "Female"
+    },
+    {
+        name: "Linda",
+        age: 60,
+        contribution: 35,
+        gender: "Female"
     }
+    
+];
+
+function sayRetirement(personData){
+    if(personData.gender === "Male") {
+        if(personData.contribution >= 35 && (personData.age + personData.contribution) >= 95)
+            console.log(`${personData.name}, you can stop working`);
+        else
+            console.log(`Sorry, ${personData.name}. You can't retire`);
+    } else {
+        if(personData.contribution >= 30 && (personData.age + personData.contribution) >= 85)
+            console.log(`${personData.name}, you can stop working`);
+        else
+            console.log(`Sorry, ${personData.name}. You can't retire`);
+    }
+}
+
+//Let's see if everybody can retire
+for(let person of persons) {
+    sayRetirement(person);
 }
